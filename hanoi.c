08e6482ffa1ac75ffadce176cc5197c094fe7539
lenkadermiskova <49731507+lenkadermiskova@users.noreply.github.com>
2021-09-25 18:51:10 +0200

    Update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("presuň kameň 1 z %d na %d\n", a, b);
+    printf("Presuň kameň 1 z %d na %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a);
-    printf("presuň kameň %d z %d na %d\n", n, a, b);
+    printf("Presuň kameň %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
